67d8a9de5bf00ce162ee78b862b41c093ddd6392
Krzysztof Rewak <10898728+krzysztofrewak@users.noreply.github.com>
2020-09-25 18:29:43 +0200

    #34 - updating Laravel to 8 (#36)
    * #34 - updating Laravel to 8
    
    * #34 - double quotes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,19 +2,24 @@
 
 declare(strict_types=1);
 
+namespace Database\Factories;
+
 use App\User;
-use Faker\Generator as Faker;
-use Illuminate\Database\Eloquent\Factory;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
-/** @var Factory $factory */
+class UserFactory extends Factory
+{
+    protected $model = User::class;
 
-$factory->define(User::class, function (Faker $faker) {
-    return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
-    ];
-});
+    public function definition(): array
+    {
+        return [
+            "name" => $this->faker->name,
+            "email" => $this->faker->unique()->safeEmail,
+            "email_verified_at" => now(),
+            "password" => "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi", // password
+            "remember_token" => Str::random(10),
+        ];
+    }
+}
