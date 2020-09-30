ef70f927ffbf54ddc33fbc9c00729964721faf9f
Adrian Gawle <61741591+agawle@users.noreply.github.com>
2020-09-30 21:52:03 +0200

    #10 set up ecs (#38)
    * #10 - Ecs added
    
    * #10 - Ecs fixes

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -3,14 +3,14 @@
 declare(strict_types=1);
 
 return [
-    'default' => env('BROADCAST_DRIVER', 'null'),
-    'connections' => [
-        'redis' => [
-            'driver' => 'redis',
-            'connection' => 'default',
+    "default" => env("BROADCAST_DRIVER", "null"),
+    "connections" => [
+        "redis" => [
+            "driver" => "redis",
+            "connection" => "default",
         ],
-        'null' => [
-            'driver' => 'null',
+        "null" => [
+            "driver" => "null",
         ],
     ],
 ];
