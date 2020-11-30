9faa8730f4a49173092fb7fb5f49853a1f5eb4bf
Adrian Gawle <adriangawle@gmail.com>
2020-11-30 21:17:13 +0100

    #11 - Auth Service

---
+++ app/Services/AuthService.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Contracts\Hashing\Hasher;
+use Illuminate\Validation\ValidationException;
+
+class AuthService
+{
+    protected Hasher $hasher;
+
+    public function __construct(Hasher $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    public function login(LoginRequest $loginRequest): string
+    {
+        $user = User::query()->where("email", $loginRequest->email)->first();
+        if (!$user || $this->hasher->check($loginRequest->password, $user->password)) {
+
+        }
+
+        return $user->createToken('auth')->plainTextToken;
+
+    }
+}
