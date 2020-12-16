29445c851e8e19d1b98d9fdbaa98d51cf41e655b
Adrian Gawle <adriangawle@gmail.com>
2020-12-16 22:12:28 +0100

    #11 - Changed handling of exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,31 +4,20 @@ declare(strict_types=1);
 
 namespace App\Exceptions;
 
+use App\Exceptions\Mapper\ExceptionMapper;
 use App\Http\Helpers\ApiResponse;
-use Illuminate\Auth\AuthenticationException;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Http\Response;
-use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Exception\RouteNotFoundException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
     public function render($request, Throwable $e)
     {
-        if ($e instanceof ApiException) {
-            return $this->renderJsonResponse($e->getMessage(), $e->getCode() ?? Response::HTTP_INTERNAL_SERVER_ERROR);
-        } elseif ($e instanceof AuthenticationException) {
-            return $this->renderJsonResponse("Unauthorized", Response::HTTP_UNAUTHORIZED);
-        } elseif ($e instanceof ModelNotFoundException || $e instanceof RouteNotFoundException) {
-            return $this->renderJsonResponse("Not found", Response::HTTP_NOT_FOUND);
-        } elseif ($e instanceof ValidationException) {
-            return $this->renderJsonResponse($e->getMessage(), Response::HTTP_UNPROCESSABLE_ENTITY, $e->errors());
-        }
+        /** @var ExceptionMapper $mapper */
+        $mapper = $this->container->make(ExceptionMapper::class);
 
-        return $this->renderJsonResponse($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        return $this->renderJsonResponse($mapper->mapMessage($e), $mapper->mapCode($e), $mapper->mapData($e));
     }
 
     protected function renderJsonResponse(string $message, int $code, array $data = []): JsonResponse
