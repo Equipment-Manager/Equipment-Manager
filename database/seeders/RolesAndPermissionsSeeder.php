e09da078bae70d66a54d43e9c9299a89a8801d8d
Adrian Gawle <adriangawle@gmail.com>
2021-01-24 19:41:29 +0100

    #14 - extended Spatie Role and Permission models to have factories

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    public function run(): void
+    {
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        Permission::create([
+            "name" => "Manage invites",
+        ]);
+
+        /** @var Role $role */
+        $role = Role::create([
+            "name" => "Admin",
+        ]);
+        $role->givePermissionTo(Permission::all());
+
+        $user = User::first();
+        $user->assignRole("Admin");
+    }
+}
