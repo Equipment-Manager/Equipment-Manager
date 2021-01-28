93e06f3dcd9d8532e6e5db7137016f475a25fc59
Adrian Gawle <adriangawle@gmail.com>
2021-01-28 20:25:47 +0100

    #14 - changed tests to use memory instead of second database

--- config/database.php
+++ config/database.php
@@ -12,15 +12,6 @@ $options = extension_loaded("pdo_mysql") ? array_filter([
 return [
     "default" => env("DB_CONNECTION", "mysql"),
     "connections" => [
-
-        "sqlite" => [
-            "driver" => "sqlite",
-            "url" => env("DATABASE_URL"),
-            "database" => env("DB_DATABASE", database_path("database.sqlite")),
-            "prefix" => "",
-            "foreign_key_constraints" => env("DB_FOREIGN_KEYS", true),
-        ],
-
         "mysql" => [
             "driver" => "mysql",
             "url" => env("DATABASE_URL"),
@@ -38,64 +29,12 @@ return [
             "engine" => null,
             "options" => $options,
         ],
-
-        "pgsql" => [
-            "driver" => "pgsql",
-            "url" => env("DATABASE_URL"),
-            "host" => env("DB_HOST", "127.0.0.1"),
-            "port" => env("DB_PORT", "5432"),
-            "database" => env("DB_DATABASE", "forge"),
-            "username" => env("DB_USERNAME", "forge"),
-            "password" => env("DB_PASSWORD", ""),
-            "charset" => "utf8",
+        "testing" => [
+            "driver" => "sqlite",
+            "database" => ":memory:",
             "prefix" => "",
-            "prefix_indexes" => true,
-            "schema" => "public",
-            "sslmode" => "prefer",
         ],
-
-        "sqlsrv" => [
-            "driver" => "sqlsrv",
-            "url" => env("DATABASE_URL"),
-            "host" => env("DB_HOST", "localhost"),
-            "port" => env("DB_PORT", "1433"),
-            "database" => env("DB_DATABASE", "forge"),
-            "username" => env("DB_USERNAME", "forge"),
-            "password" => env("DB_PASSWORD", ""),
-            "charset" => "utf8",
-            "prefix" => "",
-            "prefix_indexes" => true,
-        ],
-
     ],
 
     "migrations" => "migrations",
-
-    "redis" => [
-
-        "client" => env("REDIS_CLIENT", "phpredis"),
-
-        "options" => [
-            "cluster" => env("REDIS_CLUSTER", "redis"),
-            "prefix" => env("REDIS_PREFIX", $prefix),
-        ],
-
-        "default" => [
-            "url" => env("REDIS_URL"),
-            "host" => env("REDIS_HOST", "127.0.0.1"),
-            "password" => env("REDIS_PASSWORD", null),
-            "port" => env("REDIS_PORT", "6379"),
-            "database" => env("REDIS_DB", "0"),
-        ],
-
-        "cache" => [
-            "url" => env("REDIS_URL"),
-            "host" => env("REDIS_HOST", "127.0.0.1"),
-            "password" => env("REDIS_PASSWORD", null),
-            "port" => env("REDIS_PORT", "6379"),
-            "database" => env("REDIS_CACHE_DB", "1"),
-        ],
-
-    ],
-
 ];
