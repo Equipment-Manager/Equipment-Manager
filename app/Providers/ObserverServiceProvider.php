32a87b0828dcc8f89c7e076ace16d97e1d550163
Adrian Gawle <adriangawle@gmail.com>
2021-02-04 19:24:38 +0100

    #125 - Changed model to spatie Role in ObserverServiceProvider

--- app/Providers/ObserverServiceProvider.php
+++ app/Providers/ObserverServiceProvider.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use App\Models\Role;
 use App\Observers\RoleObserver;
 use Illuminate\Support\ServiceProvider;
 use RuntimeException;
+use Spatie\Permission\Models\Role;
 
 class ObserverServiceProvider extends ServiceProvider
 {
